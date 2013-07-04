9632ac413de0c502ce11a85a3c60eadac0d10def
novikov <novikov@inventos.ru>
2013-07-04 11:05:08 +0400

    run hls thumbnails generation in multithread mode

--- lib/video_screenshoter/hls.rb
+++ lib/video_screenshoter/hls.rb
@@ -13,18 +13,22 @@ module VideoScreenshoter
         chunk_limits.push({:start => time, :end => time + chunk.first, :path => chunk.last})
         time + chunk.first
       end
+      threads = []
       times.each do |time|
-        if chunk_limit = chunk_limits.select { |c| time >= c[:start] && time <= c[:end] }.first
-          path = File.join(File.dirname(input), chunk_limit[:path])
-          rel_time = time - chunk_limit[:start]
-          cmd = ffmpeg_command(path, output_fullpath(time), rel_time)
-          puts cmd if verbose
-          `#{cmd}`
-          imagemagick_run output_fullpath(time)
-        else
-          puts "Time #{time} is incorrect" if verbose
+        threads << Thread.new do
+          if chunk_limit = chunk_limits.select { |c| time >= c[:start] && time <= c[:end] }.first
+            path = File.join(File.dirname(input), chunk_limit[:path])
+            rel_time = time - chunk_limit[:start]
+            cmd = ffmpeg_command(path, output_fullpath(time), rel_time)
+            puts cmd if verbose
+            `#{cmd}`
+            imagemagick_run output_fullpath(time)
+          else
+            puts "Time #{time} is incorrect" if verbose
+          end
         end
       end
+      threads.each { |t| t.join  }
     end
 
     alias :make_screenshots :run
